7387263a79ddba7fdf60156ebdddfb095d616213
DanilFarkhutdinov <91538529+DanilFarkhutdinov@users.noreply.github.com>
2022-12-20 20:31:51 +0300

    Add files via upload

---
+++ InverseMatrix.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Класс_матриц
+{
+    internal class InverseMatrix: SquareMatrix
+    {
+        public InverseMatrix()
+        {
+
+        }
+        public InverseMatrix(int n) : base(n)
+        {
+
+        }
+        public InverseMatrix(int n, string c) : base(n, c)
+        {
+
+        }
+        public InverseMatrix(int n, int a = 1)
+        {
+            this.N = n;
+            this.M = n;
+            this.Array = new float[this.N, this.M];
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        this.Array[i, j] = 1;
+                    }
+                    else
+                    {
+                        this.Array[i, j] = 0;
+                    }
+                }
+            }
+        }
+        public void Inverse_matrix()
+        {
+            InverseMatrix B = new InverseMatrix(this.N);
+            for (int i = 0; i < this.N; i++)
+            {
+                for(int j = 0; j < this.N; j++)
+                {
+                    if ((i + j + 2) % 2 == 0)
+                    {
+                        B.Array[j, i] = Minor(this, i, j);
+                    }
+                    else
+                    {
+                        if (Minor(this, i, j) == 0)
+                        {
+                            B.Array[j, i] = 0;
+                        }
+                        else
+                        {
+                            B.Array[j, i] = (-1) * Minor(this, i, j);
+                        }                     
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат:");
+            for (int i = 0; i < B.N; i++)
+            {
+                for (int j = 0; j < B.M; j++)
+                {
+                    Console.Write("{0}/{1},", B.Array[i, j], Determinant_of_a_square_matrix(this));
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Проверка:");
+            this.ShowMatrix();
+            Console.WriteLine("*");
+            for (int i = 0; i < B.N; i++)
+            {
+                for (int j = 0; j < B.M; j++)
+                {
+                    Console.Write("{0}/{1},", B.Array[i, j], Determinant_of_a_square_matrix(this));
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат:");
+            Matrix C = this * B;
+            C.Division_by_scalar(Determinant_of_a_square_matrix(this));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Numerics;
+using Класс_матриц;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Matrix M = new Matrix(2, 2);
+            //M.ShowMatrix();
+            //Console.WriteLine();
+            //Matrix Z = new Matrix(3, 3, '0');
+            //Z.ShowMatrix();
+            //Console.WriteLine();
+            //Matrix R = new Matrix(3, 3, 1, 10);
+            //R.ShowMatrix();
+            //Console.WriteLine();
+            //Matrix F = new Matrix("Matrix.txt");
+            //F.ShowMatrix();
+            Console.WriteLine("1. Умножение матрицы на константу");
+            Console.WriteLine("2. Деление матрицы на константу");
+            Console.WriteLine("3. Cложение матриц");
+            Console.WriteLine("4. Вычитание матриц");
+            Console.WriteLine("5. Умножение матриц с проверкой размерностей");
+            Console.WriteLine("6. Транспонирование матрицы");
+            Console.WriteLine("7. Вывод элемента матрицы");
+            Console.WriteLine("8. Найти определитель квадратной матрицы");
+            Console.WriteLine("9. Удалить линейно зависимые строки матрицы");
+            Console.WriteLine("10. Удалить линейно зависимые строки квадратной матрицы");
+            Console.WriteLine("11. Нахождение обратной матрицы");
+            Console.WriteLine("Введите номер задачи: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (n)
+            {
+                case 1:
+                    Matrix R = new Matrix(3, 4, 1, 10);
+                    R.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Введите константу:");
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    R.Multiplication_by_scalar(c);
+                    break;
+                case 2:
+                    Matrix R1 = new Matrix(3, 4, 1, 10);
+                    R1.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Введите константу:");
+                    int c1 = Convert.ToInt32(Console.ReadLine());
+                    R1.Division_by_scalar(c1);
+                    break;
+                case 3:
+                    Matrix R2 = new Matrix(3, 4, 1, 10);
+                    R2.ShowMatrix();
+                    Console.WriteLine("+");
+                    Matrix R3 = new Matrix(3, 3, 1, 10);
+                    R3.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    Matrix A = R2 + R3;
+                    A.ShowMatrix();
+                    if (A is SquareMatrix)
+                    {
+                        Console.WriteLine("Square Matrix");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Matrix");
+                    }
+                    break;
+                case 4:
+                    Matrix R4 = new Matrix(3, 4, 1, 10);
+                    R4.ShowMatrix();
+                    Console.WriteLine("-");
+                    Matrix R5 = new Matrix(3, 4, 1, 10);
+                    R5.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    Matrix S = R4 - R5;
+                    S.ShowMatrix();
+                    if (S is SquareMatrix)
+                    {
+                        Console.WriteLine("Square Matrix");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Matrix");
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("Введите первую матрицу:");
+                    Matrix M = new Matrix(3, 4, 1, 10);
+                    Console.WriteLine();
+                    Console.WriteLine("Введите вторую матрицу:");
+                    Matrix M1 = new Matrix(4, 3, 1, 10);
+                    Console.WriteLine();
+                    M.ShowMatrix();
+                    Console.WriteLine("*");
+                    M1.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    Matrix M2 = M * M1;
+                    M2.ShowMatrix();
+                    if (M2 is SquareMatrix)
+                    {
+                        Console.WriteLine("Square Matrix");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Matrix");
+                    }
+                    break;
+                case 6:
+                    Matrix R6 = new Matrix(2, 3, 1, 10);
+                    R6.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    R6.Matrix_transposition();
+                    break;
+                case 7:
+                    Matrix R7 = new Matrix(3, 4, 1, 10);
+                    R7.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Введите индекс строки:");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Введите индекс столбца:");
+                    int j = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    R7.Output_of_the_matrix_element(i, j);
+                    break;
+                case 8:
+                    SquareMatrix SR = new SquareMatrix(3, 1, 10);
+                    SR.ShowMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Результат:");
+                    SR.Show_determinant();
+                    break;
+                case 9:
+                    Matrix X = new Matrix(3, 4, 1, 10);
+                    X.ShowMatrix();
+                    Console.WriteLine();
+                    X.Show_Gauss();
+                    break;
+                case 10:
+                    SquareMatrix Y = new SquareMatrix(3, "keyboard");
+                    Y.ShowMatrix();
+                    if (Y.Checking_determinant() == 0)
+                    {
+                        Y.Show_Gauss();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Определитель матрицы не равен 0");
+                    }
+                    break;
+                case 11:
+                    InverseMatrix IR2 = new InverseMatrix(3, 1);
+                    IR2.Inverse_matrix();
+                    break;
+            }
+        }
+    }
+}
